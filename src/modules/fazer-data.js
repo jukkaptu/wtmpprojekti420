3d1ce76c3f605e3d21d91cb398d25fd27c1e2f5a
J-P <jukka-pekka.tuomanen@metropolia.fi>
2022-03-13 19:32:59 +0200

    fazeri

---
+++ src/modules/fazer-data.js
@@ -0,0 +1,31 @@
+import {todayISODate, todayDate} from "./tools";
+
+
+const dataUrlFi = `https://www.foodandco.fi/api/restaurant/menu/week?language=fi&restaurantPageId=270540&weekDate=${todayISODate}`;
+const dataUrlEn = `https://www.foodandco.fi/api/restaurant/menu/week?language=en&restaurantPageId=270540&weekDate=${todayISODate}`;
+
+// const dataUrlFin = "https://www.foodandco.fi/api/restaurant/menu/week?language=fi&restaurantPageId=270540&weekDate=" + todayDate;
+// const dataUrlEng = "https://www.foodandco.fi/api/restaurant/menu/week?language=en&restaurantPageId=270540&weekDate=" + todayDate;
+
+/**
+ * Parses Fazer json data to simple array of strings
+ *
+ * @param {Array} lunchMenus lunch menu data
+ * @param {Number} dayOfWeek 0-6
+ * @returns {Array} daily menu
+ */
+const parseDayMenu = (lunchMenus, dayOfWeek) => {
+  const dayMenu = lunchMenus[dayOfWeek].SetMenus.map(setMenu => {
+    const name = setMenu.Name;
+    let meals = '';
+    // TODO: clean output
+    for (const meal of setMenu.Meals) {
+      meals += meal.Name + ', ';
+    }
+    return  name ? name + ': ' + meals : meals;
+  });
+  return dayMenu;
+};
+
+const FazerData = {parseDayMenu, dataUrlFi, dataUrlEn};
+export default FazerData;
